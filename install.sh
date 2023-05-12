f405b40d29b9a4d924dfcfeca5d0a351004c40db
Dragonflame7155 <nrgmyob@gmail.com>
2023-05-12 16:24:23 -0400

    bruh moment

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 cp init.lua ~/.config/nvim/
+mkdir ~/.config/nvim/lua
+cp diagnostics.lua lspconfig-config.lua lspconfig.lua lspconfigs.config lualine-config.lua nvim-cmp-config.lua nvim-tree-config.lua telescope-config.lua treesitter-config.lua ~/.config/nvim/lua/
